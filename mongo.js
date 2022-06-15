fa3fc1ad555a900e808533e187d51523d8485de2
Risto Koverola <risto.koverola@eficode.com>
2022-06-15 15:45:38 +0300

    Save progress on notes

---
+++ mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.tbz9x.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    Note.find({}).then(result => {
+        result.forEach(note => {
+          console.log(note)
+        })
+        mongoose.connection.close() 
+    })
+  })
+  .catch((err) => console.log(err))
