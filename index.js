56f3489e42a601d40405c9751a0c5d1c24f3eebb
Risto Koverola <risto.koverola@eficode.com>
2022-06-16 10:09:13 +0300

    Make changes up until 3.13

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 app.use(express.json())
@@ -16,6 +19,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
+// FIXME: Obsolete?
 let notes = [
   {
     id: 1,
@@ -42,19 +46,18 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(n => n.id === id)
-  if(note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
+// TODO: Convert to MongoDB
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(n => n.id !== id)
@@ -78,16 +81,17 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
   console.log('Got post request ->', note)
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
+  
 })
 
 const unknownEndpoint = (request, response) => {
@@ -95,7 +99,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
 })
---
+++ models/note.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('Connecting to ', url)
+mongoose.connect(url)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('Error when connecting: ', error.message)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+  })
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+  
+const Note = mongoose.model('Note', noteSchema)
+module.exports = Note
